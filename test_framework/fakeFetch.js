c3a1c1a255fae5c449ec1472ffc50c1dde8d4631
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-11-18 09:53:27 +0000

    updated with body data

--- test_framework/fakeFetch.js
+++ test_framework/fakeFetch.js
@@ -7,14 +7,22 @@
         results:
         [
           {
-            'id': '1',
-            'webTitle': 'this is headline 1',
-            'webUrl': 'https://url.com'
+            id: '1',
+            fields:
+              {
+                body: '<p>P1</p><p>P2</p><p>P3</p><p>P4</p><p>P5</p>'
+              },
+            webTitle: 'this is headline 1',
+            webUrl: 'https://url.com'
           },
           {
-            'id': '2',
-            'webTitle': 'this is headline 2',
-            'webUrl': 'https://url.com'
+            id: '2',
+            fields:
+              {
+                body: '<p>P1</p><p>P2</p><p>P3</p><p>P4</p><p>P5</p>'
+              },
+            webTitle: 'this is headline 2',
+            webUrl: 'https://url.com'
           }
         ]
       }
